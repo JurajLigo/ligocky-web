ec039d44da0d28bfcf70f8cb1476f56794065107
j.ligocky <j.ligocky@gmail.com>
2019-01-24 22:09:28 +0100

    add flag to travel story

--- src/box/box.tsx
+++ src/box/box.tsx
@@ -3,12 +3,15 @@ import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import './box.scss';
 
+const Flag = require("react-flags");
+
 interface BoxProps {
     title: string;
     imagePath: string;
     text: string;
     isTextFirst: boolean;
     link: string;
+    flag?: string;
 }
 
 export default class Box extends React.Component<BoxProps> {
@@ -23,47 +26,49 @@ export default class Box extends React.Component<BoxProps> {
         return (
             <div className="box__content max-width">
                 {isTextFirst && <>
-                    <div className="box__col">
-                        <img className="box__picture" src={this.props.imagePath}/>
-                        <h2 className="box__title">{this.props.title}</h2>
-                        <div className="box__description-wrapper">
+                  <div className="box__col">
+                    <img className="box__picture" src={this.props.imagePath}/>
+                    <h2 className="box__title">{this.props.title}</h2>
+                    <div className="box__description-wrapper">
                             <span className="box__place">
                                 <FontAwesomeIcon icon="map-marker-alt" size="1x" className="icon"/>
                                 Havana</span>
-                            <span className="box__time"><FontAwesomeIcon icon="clock" size="1x" className="icon"/>február 2018</span>
-                        </div>
+                      <span className="box__time"><FontAwesomeIcon icon="clock" size="1x" className="icon"/>február 2018</span>
                     </div>
-                    <div className="box__col">
-                        <p className="box__text">{this.props.text}</p>
-                        <p>
-                            <Link to={this.props.link} className="link"><FontAwesomeIcon icon={['fab', 'readme']}
-                                                                                         size="lg"
-                                                                                         className="icon"/>Čítať
-                                viac</Link>
-                        </p>
-                    </div>
+                  </div>
+                  <div className="box__col">
+                    <p className="box__text">{this.props.text}</p>
+                    <p>
+                      <Link to={this.props.link} className="link"><FontAwesomeIcon icon={['fab', 'readme']}
+                                                                                   size="lg"
+                                                                                   className="icon"/>Čítať
+                        viac</Link>
+                    </p>
+                  </div>
                 </>}
 
                 {!isTextFirst && <>
-                    <div className="box__col">
-                        <img className="box__picture" src={this.props.imagePath}/>
-                    </div>
-                    <div className="box__col">
-                        <h2 className="box__title">{this.props.title}</h2>
-                        <div className="box__description-wrapper">
+                  <div className="box__col">
+                    <img className="box__picture" src={this.props.imagePath}/>
+                  </div>
+                  <div className="box__col">
+                    <h2 className="box__title">{this.props.flag && <Flag
+                      name={this.props.flag} format="png" basePath="assets" pngSize={32} shiny={false}
+                      className="header__flag"/>}{this.props.title}</h2>
+                    <div className="box__description-wrapper">
                             <span className="box__place">
                                 <FontAwesomeIcon icon="map-marker-alt" size="1x" className="icon"/>
                                 Havana</span>
-                            <span className="box__time"><FontAwesomeIcon icon="clock" size="1x" className="icon"/>február 2018</span>
-                        </div>
-                        <p className="box__text">{this.props.text}</p>
-                        <p>
-                            <Link to={this.props.link} className="link"><FontAwesomeIcon icon={['fab', 'readme']}
-                                                                                         size="lg"
-                                                                                         className="icon"/>Čítať
-                                viac</Link>
-                        </p>
+                      <span className="box__time"><FontAwesomeIcon icon="clock" size="1x" className="icon"/>február 2018</span>
                     </div>
+                    <p className="box__text">{this.props.text}</p>
+                    <p>
+                      <Link to={this.props.link} className="link"><FontAwesomeIcon icon={['fab', 'readme']}
+                                                                                   size="lg"
+                                                                                   className="icon"/>Čítať
+                        viac</Link>
+                    </p>
+                  </div>
                 </>}
             </div>
         );
--- src/travelStory/travelStoryContainer.tsx
+++ src/travelStory/travelStoryContainer.tsx
@@ -26,6 +26,7 @@ export default class TravelStoryContainer extends React.Component<TravelStoryCon
                         text="Prvý deň na v Havane bol dlhy a veľkolepý, vstali sme o 6 ráno a preskúmali toto neskutočné mesto. Večer sme sa opili a vtrhli na miestnu diskotéku. Keď už sme sa vrátili na izbu zistil som ze mi chýba jeden mobil. Mal som dva mobily, jeden môj a druhy pracovný, ktorý som chcel použiť na fotografovanie pamiatok a iných zaujímavosti :) Okolo 4:30 AM som zistil ze nemám ten firemný :( Radšej som teda zaspal a dúfal som ze ráno sa už zobudím s telefónom. Na moje počudovanie sa ráno zobúdzam a počet telefónov je stále 1 :) ..."
                         title="Interakcia s kubánskou políciou"
                         link="travelstories/havana"
+                        flag="CU"
                         />
                     <Box 
                         isTextFirst={false} 
@@ -33,6 +34,7 @@ export default class TravelStoryContainer extends React.Component<TravelStoryCon
                         text="Po jemnom preskúmaní hlavného námestia v centre Santa Clary sme si všimli, že na okraji je jedna reštaurácia pred ktorou stojí v rade strašne veľa miestnych. Pohľadmi sme teda preskúmali vzhľad reštaurácie a keďže sme nemali čas čakať rozhodli sme sa, že si urobíme rezerváciu na večeru ..."
                         title="Gurmanský zážitok v Santa Clare"
                         link="travelstories/santa-clara"
+                        flag="CU"
                         />
                     <Box 
                         isTextFirst={false} 
@@ -40,6 +42,7 @@ export default class TravelStoryContainer extends React.Component<TravelStoryCon
                         text="Boli sme už dosť južne, aj na úrovni Sahary ale jemne sa ťaháme k moru. Cesty sú takmer prázdne , skoro žiadna doprava. Okolie som sa začal svedomitejšie sledovať, nakoľko zeleň na zemi ubúdala až sa stratila a držala sa už iba na stromoch. To znamenalo jedine bliží sa marocká, stromová atrakcia, o ktorej som si čítal pred odletom - Kozy na stromoch ..."
                         title="Marocké kozy na stromoch"
                         link="travelstories/morocco"
+                        flag="MAR"
                         />
                 </div>
             </>
