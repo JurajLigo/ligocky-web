2865c241523ab496d201d6ba14e8b03706ee1970
j.ligocky <j.ligocky@gmail.com>
2019-01-29 22:51:07 +0100

    cv + travel story css refactor px to rem

--- src/cv/cv.tsx
+++ src/cv/cv.tsx
@@ -8,13 +8,12 @@ import Work from './work';
 export default function Cv(): JSX.Element {
 
     return (
-        <>
+        <div className="cv">
             <Hero imagePath="../../assets/cv/cv_6.jpg" mobileImagePath="../../assets/cv/cv_mobile.jpg"
                   mobilePortraitImagePath="../../assets/cv/cv_mobile_portrait.jpg" title="Curiculum vitae"/>
             <Work/>
             <Education/>
             <Hobby/>
-
-        </>
+        </div>
     );
 }
