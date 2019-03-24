8546273d6e5d6fb35a8cd8cc210254793d417e4d
Juraj Ligocky <j.ligocky@gmail.com>
2019-03-24 21:43:39 +0100

    skill box - continue styling box

--- src/cv/skill.tsx
+++ src/cv/skill.tsx
@@ -10,10 +10,36 @@ export default function Skill(): JSX.Element {
             <div className="skill">
                 <div className="skill__box">
                     <img src="../../assets/cv/skill/javascript.png"/>
-                    <h1 className="skill__box-title skill__box-title--javascript">Javasript</h1>
+                    <h1 className="skill__box-title skill__box-title--javascript">javascript</h1>
+                    <div className="skill__box-text">React</div>
+                    <div className="skill__box-text">ES 5, ES 6</div>
+                    <div className="skill__box-text">Typescript</div>
+                    <div className="skill__box-text">Redux</div>
                 </div>
-                <div className="skill__box">Java</div>
-                <div className="skill__box">Other</div>
+                <div className="skill__box">
+                    <img src="../../assets/cv/skill/java.png"/>
+                    <h1 className="skill__box-title skill__box-title--java">Java</h1>
+                    <div className="skill__box-text">React</div>
+                    <div className="skill__box-text">ES 5, ES 6</div>
+                    <div className="skill__box-text">Typescript</div>
+                    <div className="skill__box-text">Redux</div>
+                </div>
+                <div className="skill__box">
+                    <img src="../../assets/cv/skill/javascript.png"/>
+                    <h1 className="skill__box-title skill__box-title--javascript">Other</h1>
+                    <div className="skill__box-text">React</div>
+                    <div className="skill__box-text">ES 5, ES 6</div>
+                    <div className="skill__box-text">Typescript</div>
+                    <div className="skill__box-text">Redux</div>
+                </div>
+                <div className="skill__box">
+                    <img src="../../assets/cv/skill/javascript.png"/>
+                    <h1 className="skill__box-title skill__box-title--javascript">Other</h1>
+                    <div className="skill__box-text">React</div>
+                    <div className="skill__box-text">ES 5, ES 6</div>
+                    <div className="skill__box-text">Typescript</div>
+                    <div className="skill__box-text">Redux</div>
+                </div>
             </div>
         </>
     );
