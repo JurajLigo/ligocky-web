1fa42e2210f7873cf5420bd73757514c8ddeef16
Juraj Ligocky <j.ligocky@gmail.com>
2019-04-13 00:22:21 +0200

    skill box - continue styling box

--- src/cv/skill.tsx
+++ src/cv/skill.tsx
@@ -10,35 +10,33 @@ export default function Skill(): JSX.Element {
             <div className="skill">
                 <div className="skill__box">
                     <img src="../../assets/cv/skill/javascript.png"/>
-                    <h1 className="skill__box-title skill__box-title--javascript">javascript</h1>
-                    <div className="skill__box-text">React</div>
-                    <div className="skill__box-text">ES 5, ES 6</div>
-                    <div className="skill__box-text">Typescript</div>
-                    <div className="skill__box-text">Redux</div>
+                    <h1 className="skill__box-title skill__box-title--javascript">Javascript</h1>
+                    <div className="skill__list">
+                        <div className="skill__box-text">React (good knwoledge)</div>
+                        <div className="skill__box-text">Redux</div>
+                        <div className="skill__box-text">ES 5, ES 6</div>
+                        <div className="skill__box-text">Typescript</div>
+                        <div className="skill__box-text">Jasmine</div>
+                    </div>
                 </div>
                 <div className="skill__box">
-                    <img src="../../assets/cv/skill/java.png"/>
+                    <img src="../../assets/cv/skill/java.svg"/>
                     <h1 className="skill__box-title skill__box-title--java">Java</h1>
-                    <div className="skill__box-text">React</div>
-                    <div className="skill__box-text">ES 5, ES 6</div>
-                    <div className="skill__box-text">Typescript</div>
-                    <div className="skill__box-text">Redux</div>
+                    <div className="skill__list">
+                        <div className="skill__box-text">J2EE (basics)</div>
+                        <div className="skill__box-text">Spring (basics)</div>
+                        <div className="skill__box-text">Swing, Wicket</div>
+                        <div className="skill__box-text">JUnit</div>
+                    </div>
                 </div>
                 <div className="skill__box">
-                    <img src="../../assets/cv/skill/javascript.png"/>
-                    <h1 className="skill__box-title skill__box-title--javascript">Other</h1>
-                    <div className="skill__box-text">React</div>
-                    <div className="skill__box-text">ES 5, ES 6</div>
-                    <div className="skill__box-text">Typescript</div>
-                    <div className="skill__box-text">Redux</div>
-                </div>
-                <div className="skill__box">
-                    <img src="../../assets/cv/skill/javascript.png"/>
-                    <h1 className="skill__box-title skill__box-title--javascript">Other</h1>
-                    <div className="skill__box-text">React</div>
-                    <div className="skill__box-text">ES 5, ES 6</div>
-                    <div className="skill__box-text">Typescript</div>
-                    <div className="skill__box-text">Redux</div>
+                    <div className="skill__other"/>
+                    <h1 className="skill__box-title skill__box-title--other">Other</h1>
+                    <div className="skill__list">
+                        <div className="skill__box-text">HTML 5 / CSS 3</div>
+                        <div className="skill__box-text">MDA</div>
+                        <div className="skill__box-text">SOA</div>
+                    </div>
                 </div>
             </div>
         </>
